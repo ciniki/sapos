8bd401654668cee644b19864f215b8f595d3e529
Andrew Rivett <veggiefrog@gmail.com>
2014-02-03 19:55:46 -0500

    Removed duplicate error codes

--- public/invoiceItemUpdate.php
+++ public/invoiceItemUpdate.php
@@ -66,7 +66,7 @@ function ciniki_sapos_invoiceItemUpdate(&$ciniki) {
 		return $rc;
 	}   
 	if( !isset($rc['item']) ) {
-		return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'1400', 'msg'=>'Unable to locate the invoice item'));
+		return array('stat'=>'fail', 'err'=>array('pkg'=>'ciniki', 'code'=>'1523', 'msg'=>'Unable to locate the invoice item'));
 	}
 	$item = $rc['item'];
 
