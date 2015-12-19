af4f8120c95f1cea6b05a0c939dc043fa0a37176
Andrew Rivett <veggiefrog@gmail.com>
2015-12-19 00:31:34 -0500

    Started work on transition to updated account page

---
+++ web/accountSessionUnload.php
@@ -0,0 +1,20 @@
+<?php
+//
+// Description
+// -----------
+// This function will check for an existing cart to load into the session
+//
+// Arguments
+// ---------
+//
+// Returns
+// -------
+//
+function ciniki_sapos_web_accountSessionUnload($ciniki, $settings, $business_id) {
+
+    unset($ciniki['session']['cart']);
+    unset($_SESSION['cart']);
+
+	return array('stat'=>'ok');
+}
+?>
