c5765e287751819a364931dd71686f8f4e446324
Andrew Rivett <veggiefrog@gmail.com>
2020-05-13 14:02:49 -0400

    Added objectList for auto category of donations

---
+++ sapos/objectList.php
@@ -0,0 +1,27 @@
+<?php
+//
+// Description
+// ===========
+// This method returns the list of objects that can be returned
+// as invoice items.
+//
+// Arguments
+// =========
+// 
+// Returns
+// =======
+//
+function ciniki_sapos_sapos_objectList($ciniki, $tnid) {
+
+    $objects = array(
+        //
+        // this object should only be added to carts
+        //
+        'ciniki.sapos.donationpackage' => array(
+            'name' => 'Donation',
+            ),
+        );
+
+    return array('stat'=>'ok', 'objects'=>$objects);
+}
+?>
