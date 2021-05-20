a1ff4584141477622396abf50be3fba0d58d6862
Andrew Rivett <veggiefrog@gmail.com>
2021-05-20 16:58:46 -0400

    Added ability for modules to disconnect object from invoice line item, but leave item on invoice for records

---
+++ hooks/invoiceItemDisconnect.php
@@ -0,0 +1,91 @@
+<?php
+//
+// Description
+// -----------
+// This function will return the list of invoices for a customer.
+//
+// Arguments
+// ---------
+// ciniki:
+// tnid:         The tenant ID to check the session user against.
+// method:              The requested method.
+//
+// Returns
+// -------
+// <rsp stat='ok' />
+//
+function ciniki_sapos_hooks_invoiceItemDisconnect($ciniki, $tnid, $args) {
+
+    if( !isset($args['invoice_id']) || $args['invoice_id'] == '' 
+        || !isset($args['object']) || $args['object'] == '' 
+        || !isset($args['object_id']) || $args['object_id'] == '' 
+        ) {
+        return array('stat'=>'fail', 'err'=>array('code'=>'ciniki.sapos.3', 'msg'=>'No invoice or item specified.'));
+    }
+
+    //
+    // Load the settings
+    //
+    $rc = ciniki_core_dbDetailsQueryDash($ciniki, 'ciniki_sapos_settings', 'tnid', $tnid, 'ciniki.sapos', 'settings', '');
+    if( $rc['stat'] != 'ok' ) {
+        return $rc;
+    }
+    $settings = isset($rc['settings'])?$rc['settings']:array();
+
+    //
+    // Get the details of the item
+    //
+    $strsql = "SELECT id, uuid, invoice_id, object, object_id, quantity "
+        . "FROM ciniki_sapos_invoice_items "
+        . "WHERE invoice_id = '" . ciniki_core_dbQuote($ciniki, $args['invoice_id']) . "' "
+        . "AND object = '" . ciniki_core_dbQuote($ciniki, $args['object']) . "' "
+        . "AND object_id = '" . ciniki_core_dbQuote($ciniki, $args['object_id']) . "' "
+        . "AND tnid = '" . ciniki_core_dbQuote($ciniki, $tnid) . "' "
+        . "";
+    $rc = ciniki_core_dbHashQuery($ciniki, $strsql, 'ciniki.sapos', 'item');
+    if( $rc['stat'] != 'ok' ) {
+        return $rc;
+    }
+    if( !isset($rc['item']) ) {
+        return array('stat'=>'fail', 'err'=>array('code'=>'ciniki.sapos.4', 'msg'=>'Unable to find invoice item'));
+    }
+    $item = $rc['item'];
+
+    //
+    // Check for a callback for the item object
+    //
+    if( $item['object'] != '' && $item['object_id'] != '' ) {
+        list($pkg,$mod,$obj) = explode('.', $item['object']);
+        $rc = ciniki_core_loadMethod($ciniki, $pkg, $mod, 'sapos', 'itemDelete');
+        if( $rc['stat'] == 'ok' ) {
+            $fn = $rc['function_call'];
+            $rc = $fn($ciniki, $tnid, $item['invoice_id'], $item);
+            if( $rc['stat'] != 'ok' ) {
+                return $rc;
+            }
+        }
+    }
+
+    //
+    // Remove the item
+    //
+    ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'objectDelete');
+    $rc = ciniki_core_objectUpdate($ciniki, $tnid, 'ciniki.sapos.invoice_item', $item['id'], array(
+        'object' => '',
+        'object_id' => '',
+        'price_id' => 0,
+        ), 0x04);
+    if( $rc['stat'] != 'ok' ) {
+        return $rc;
+    }
+
+    //
+    // Update the last_change date in the tenant modules
+    // Ignore the result, as we don't want to stop user updates if this fails.
+    //
+    ciniki_core_loadMethod($ciniki, 'ciniki', 'tenants', 'private', 'updateModuleChangeDate');
+    ciniki_tenants_updateModuleChangeDate($ciniki, $tnid, 'ciniki', 'sapos');
+
+    return array('stat'=>'ok');
+}
+?>
